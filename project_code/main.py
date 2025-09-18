08505c95be68ff12043588c85fb04fe3edaa8240
odyssey <bhartipranav8@gmail.com>
2025-09-18 07:12:04 +0530

    fixed esc to work regarless of when you orignially pressed esc

--- project_code/main.py
+++ project_code/main.py
@@ -12,11 +12,23 @@ import time
 keyboard_events = []
 mouse_events = []
 
+def custom_keyboard_record():
+    # similar to keyboard.record(), but with a few changes
+    def append_event(event): # TODO add hotkey support for stuff like shift+a
+        nonlocal recorded
+        recorded.append((event,(None,None))) # None says that the x and y coords are not taken for keyboard inputs (unlike mouse)
+
+    recorded = []
+    keyboard.hook(append_event)
+    keyboard.wait("esc")
+    keyboard.unhook(append_event)
+    recorded = recorded[:-1] # removes the esc exit key from events
+    return recorded
+
 def record_keyboard():
     global keyboard_events
     print("Recording keyboard... Press ESC to stop.")
-    keyboard_events = keyboard.record(until = "esc")
-    keyboard_events = keyboard_events[:-1] # removes the esc exit key from
+    keyboard_events = custom_keyboard_record()
     print("Keyboard recording stopped.")
     stop_mouse.set() # tells custom_mouse_record() to stop recording
 
@@ -36,11 +48,11 @@ def custom_mouse_record():
 
 def record_mouse():
     global mouse_events
-    print("Recording mouse... Right click to stop.")
+    print("Recording mouse... Press ESC to stop.")
     mouse_events = custom_mouse_record()
     print("Mouse recording stopped.")
 
-# main_code
+# recording the macro
 keyboard_thread = threading.Thread(target=record_keyboard)
 mouse_thread = threading.Thread(target=record_mouse)
 stop_mouse = threading.Event()
@@ -60,30 +72,47 @@ print("\nRecording finished! Duration: " + str(round(end_time - start_time,2)) +
 # merging into one list named combined_events
 combined_events = []
 
-for event in keyboard_events:
-    combined_events.append(("keyboard", event.time, event, (None,None))) # None says that the x and y coords are not taken for keyboard inputs (obv)
+for item in keyboard_events: # in the format of a tuple (event,(None,None))
+    event = item[0]
+    coordinates = item[1]
+    combined_events.append(("keyboard", event.time, event, coordinates)) # None says that the x and y coords are not taken for keyboard inputs (obviously)
 
-for event in mouse_events:
-    combined_events.append(("mouse", event[0].time, event[0],event[1]))
+for item in mouse_events: # in the format of a tuple (event,(x_pos,y_pos))
+    event = item[0]
+    coordinates = item[1]
+    combined_events.append(("mouse", event.time, event,coordinates))
 
 # sort by timestamp
 combined_events.sort(key=lambda x: x[1])
 
-print(f"\n--- Combined Timeline ("+ str(len(combined_events)) + " events) ---")
+print("\n--- Combined Timeline ("+ str(len(combined_events)) + " events) ---")
 for source, timestamp, event, (pos_x,pos_y) in combined_events:
     print(str(round(timestamp,4)), source, event, str(pos_x) + "," + str(pos_y), sep = " | ")
 
+def detect_escape():
+    keyboard.wait("esc") # will not progress till esc is hit
+    escape_found.set()
+
+
 # playing back the macro
-print("\nReplaying in 3 seconds... Hold ESC to quit at any time!")
+print("\nReplaying in 3 seconds... Press ESC to quit at any time!")
 time.sleep(3)
 
-start_playback = time.time()
+try:
+    prev_t = combined_events[0][1] # orignially, first timestamp is the prev t
+except IndexError:
+    print("\nNo events detected!")
 
-prev_t = combined_events[0][1] # orignially, first timestamp is the prev t
 replay_speed = 1
+
+escape_detector = threading.Thread(target = detect_escape) # exit detector
+escape_detector.start()
+escape_found = threading.Event()
+
 for src, t, ev, (pos_x,pos_y) in combined_events:
-    if keyboard.is_pressed('esc'):
+    if escape_found.is_set():
         break
+
     delay = (t - prev_t)/replay_speed # this makes it real time, but then the computer struggles to do combinations like shift-a
     prev_t = t
     if delay > 0:
@@ -97,4 +126,5 @@ for src, t, ev, (pos_x,pos_y) in combined_events:
         else: # scrolling doesnt work on ubuntu 24 - idk why
             mouse.play([ev])
 
+escape_detector.join()
 print("\nPlayback finished.")
